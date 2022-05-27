dfa68a336a661d0b18d5a33d55a2217d4b05fd1d
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-27 09:57:30 +0300

    fourOperation java class complied

---
+++ HelloWorldProject/src/FourOperation.java
@@ -0,0 +1,30 @@
+
+public class FourOperation {
+
+	public static void main(String[] args) {
+		System.out.println("Summing = "+summing(5,2));
+		System.out.println("Extraction = "+extraction(5,2));
+		System.out.println("Multiplication = "+multiplication(5,2));
+		System.out.println("Division = "+division(5,2));
+
+		
+
+	}
+	
+	public static double summing(double a, double b) {
+		return a + b;
+	}
+	
+	public static double extraction(double a, double b) {
+		return a - b;
+	}
+	
+	public static double multiplication(double a, double b) {
+		return a * b;
+	}
+	
+	public static double division(double a, double b) {
+		return a / b;
+	}
+
+}
---
+++ HelloWorldProject/src/HelloWorldMethod.java
@@ -0,0 +1,13 @@
+
+public class HelloWorldMethod {
+
+	public static void main(String[] args) {
+		hello();
+	}
+	
+	public static void hello() {
+		System.out.println("HelloWorld");
+
+	}
+
+}
