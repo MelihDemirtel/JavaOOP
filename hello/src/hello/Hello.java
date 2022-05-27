00e9f6af269b79a2daef85e5444f62ed4bfbe8ab
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-27 10:44:44 +0300

    hello netbeans complied

---
+++ hello/src/hello/Hello.java
@@ -0,0 +1,19 @@
+package hello;
+
+/**
+ *
+ * @author MelihDemirtel
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        System.out.println("Hello World");
+
+    }
+    
+}
