ff42f9a1d551aa633707ac9668e968467a4fb72a
MelihDemirtel <melihcandemirtel@gmail.com>
2022-05-30 14:50:22 +0300

    groceryApp project is over

--- GroceryApp/src/groceryapp/GroceryApp.java
+++ GroceryApp/src/groceryapp/GroceryApp.java
@@ -16,23 +16,30 @@ public class GroceryApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        grocery();
         
     }
     
-    public static void info(){
+    public static void grocery(){
         Scanner input = new Scanner(System.in);
         Stock stock = new Stock();
                 
-        System.out.println("How many Apple KG = ");
+        System.out.print("How many Apple KG = ");
         double kilogramApple = input.nextDouble();
+        stock.setKilogramApple(kilogramApple);
+        System.out.println("Apple = "+stock.getApplePrice()+"TL");
         
-        System.out.println("How many Pear KG = ");
+        System.out.print("How many Pear KG = ");
         double kilogramPear = input.nextDouble();
+        stock.setKilogramPear(kilogramPear);
+        System.out.println("Pear = "+stock.getPearPrice()+"TL");
         
-        System.out.println("How many Orange KG = ");
+        System.out.print("How many Orange KG = ");
         double kilogramOrange = input.nextDouble();
+        stock.setKilogramOrange(kilogramOrange);
+        System.out.println("Orange = "+stock.getOrangePrice()+"TL");
         
+        System.out.print("Total Amount = "+stock.getTotal()+"TL"); 
         
     }
     
