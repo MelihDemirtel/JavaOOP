e1dbb0c3ea752faa195d9db1f3123b43a6072646
MelihDemirtel <melihcandemirtel@gmail.com>
2022-06-12 14:32:45 +0300

    cafeApp project is over

--- CafeApp/src/Menu.java
+++ CafeApp/src/Menu.java
@@ -25,21 +25,99 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        chcTea = new javax.swing.JCheckBox();
+        chcOrange = new javax.swing.JCheckBox();
+        chcWater = new javax.swing.JCheckBox();
+        chcCola = new javax.swing.JCheckBox();
+        chcCoffe = new javax.swing.JCheckBox();
+        btnCalculate = new javax.swing.JButton();
+        lblAmount = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        chcTea.setText("Tea - 3 TL");
+
+        chcOrange.setText("Orange Juice - 6 TL");
+
+        chcWater.setText("Water - 1 TL");
+
+        chcCola.setText("Cola - 10 TL");
+
+        chcCoffe.setText("Coffe - 12 TL");
+
+        btnCalculate.setText("Calculate");
+        btnCalculate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCalculateActionPerformed(evt);
+            }
+        });
+
+        lblAmount.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblAmount.setText("Amount : 0 TL");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(chcTea, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(chcOrange, javax.swing.GroupLayout.DEFAULT_SIZE, 133, Short.MAX_VALUE)
+                            .addComponent(chcWater, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(chcCola, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(chcCoffe, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnCalculate)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 105, Short.MAX_VALUE)
+                        .addComponent(lblAmount)
+                        .addGap(80, 80, 80))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(13, 13, 13)
+                .addComponent(chcTea)
+                .addGap(18, 18, 18)
+                .addComponent(chcOrange)
+                .addGap(18, 18, 18)
+                .addComponent(chcWater)
+                .addGap(18, 18, 18)
+                .addComponent(chcCola)
+                .addGap(18, 18, 18)
+                .addComponent(chcCoffe)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnCalculate)
+                    .addComponent(lblAmount))
+                .addContainerGap(71, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCalculateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCalculateActionPerformed
+        int amount = 0;
+        if(chcTea.isSelected()){
+            amount = amount + 3;
+        }
+        if(chcCoffe.isSelected()){
+            amount = amount + 12;
+        }
+        if(chcOrange.isSelected()){
+            amount = amount + 6;
+        }
+        if(chcWater.isSelected()){
+            amount = amount + 1;
+        }
+        if(chcCola.isSelected()){
+            amount = amount + 10;
+        }
+        lblAmount.setText("Amount : "+amount+" TL");
+    }//GEN-LAST:event_btnCalculateActionPerformed
 
     /**
      * @param args the command line arguments
@@ -77,5 +155,12 @@ public class Menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCalculate;
+    private javax.swing.JCheckBox chcCoffe;
+    private javax.swing.JCheckBox chcCola;
+    private javax.swing.JCheckBox chcOrange;
+    private javax.swing.JCheckBox chcTea;
+    private javax.swing.JCheckBox chcWater;
+    private javax.swing.JLabel lblAmount;
     // End of variables declaration//GEN-END:variables
 }
